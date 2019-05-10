278f95e47b1fcc7fc142419fa04bee89caeab6e0
book_swag <its.bookswag@gmail.com>
2019-05-10 22:42:44 +0900

    [func&lambda] do lambda with Function<T,R>

--- lambda/src/main/java/com/bookswag/funcp/ch1/App2.java
+++ lambda/src/main/java/com/bookswag/funcp/ch1/App2.java
@@ -1,16 +1,13 @@
 package com.bookswag.funcp.ch1;
 
+import java.util.function.Function;
+
 public class App2 {
     public static void main(String[] args) {
         System.out.println(process("Hello, World!", str -> str.toUpperCase()));
     }
 
-    private static String process(String hello, Processor processor) {
-        return processor.process(hello);
+    private static String process(String hello, Function<String, String> processor) {
+        return processor.apply(hello);
     }
-}
-
-@FunctionalInterface
-interface Processor {
-    String process(String str);
 }
