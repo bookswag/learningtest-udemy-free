9a21d5f98161ff458fdecdc3d05f186b365241a0
book_swag <its.bookswag@gmail.com>
2019-05-13 22:28:42 +0900

    [multithreading] start for multiple locks

---
+++ multithreading/src/main/java/com/bookswag/thread/ch4/App.java
@@ -0,0 +1,7 @@
+package com.bookswag.thread.ch4;
+
+public class App {
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+}
---
+++ multithreading/src/main/java/com/bookswag/thread/ch4/Worker.java
@@ -0,0 +1,7 @@
+package com.bookswag.thread.ch4;
+
+public class Worker {
+    public void main() {
+        System.out.println("Hello, I'm Worker");
+    }
+}
